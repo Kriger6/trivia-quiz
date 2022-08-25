0d55c7b8c908be0d3bd249a51a877389ad146ba0
Kriger6 <sasa.palinkas6@gmail.com>
2022-08-25 22:00:10 +0200

    initial commit

---
+++ src/components/registration/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Registration = () => {
+  return (
+    <div>
+        <div className='photo-container'>
+
+        </div>
+        <div className='reg-card-container'>
+            <div className='reg-card'>
+        
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Registration
