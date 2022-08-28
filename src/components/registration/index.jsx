da7770c15f0155008d54e98bf990afdff538296b
Kriger6 <sasa.palinkas6@gmail.com>
2022-08-28 22:08:54 +0200

    more work done

--- src/components/registration/index.jsx
+++ src/components/registration/index.jsx
@@ -3,13 +3,20 @@ import React from 'react'
 const Registration = () => {
   return (
     <div>
-        <div className='photo-container'>
-
-        </div>
-        <div className='reg-card-container'>
-            <div className='reg-card'>
-        
+        <div className='section-container'>
+          <div className='left-side-container'>
+            <h1>trivia</h1>
+            <div className='photo-container'>
+              
             </div>
+          </div>
+          <div className='right-side-container'>
+            <div className='reg-card-container'>
+                <div className='reg-card'>
+
+                </div>
+            </div>
+          </div>
         </div>
     </div>
   )
