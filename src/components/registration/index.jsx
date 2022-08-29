1a02f78eb116368f0165dcea04433787f156bfb8
Kriger6 <sasa.palinkas6@gmail.com>
2022-08-29 11:50:27 +0200

    more work done

--- src/components/registration/index.jsx
+++ src/components/registration/index.jsx
@@ -3,21 +3,36 @@ import React from 'react'
 const Registration = () => {
   return (
     <div>
-        <div className='section-container'>
-          <div className='left-side-container'>
-            <h1>trivia</h1>
-            <div className='photo-container'>
-              
-            </div>
+      <div className='section-container'>
+        <div className='left-side-container'>
+          <h1>trivia</h1>
+          <div className='photo-container'>
+            <img src={require('../../assets/quiz.png')} alt='quiz logo' className='img' />
           </div>
-          <div className='right-side-container'>
-            <div className='reg-card-container'>
-                <div className='reg-card'>
-
-                </div>
+        </div>
+        <div className='right-side-container'>
+          <div className='reg-card-container'>
+            <div className='reg-card-header'>
+              <h1>
+                Sign up
+              </h1>
+              <p>Fast and simple</p>
+            </div>
+            <div className='reg-card'>
+              <form>
+                <input type='text' placeholder='Name'></input>
+                <input type='text' placeholder='Last Name'></input>
+                <input type='text' placeholder='Nick'></input>
+                <input type='text' placeholder=''></input>
+                <input type='radio'></input>
+                <input type='radio'></input>
+                <input type='radio'></input>
+                <button>Sign up</button>
+              </form>
             </div>
           </div>
         </div>
+      </div>
     </div>
   )
 }
