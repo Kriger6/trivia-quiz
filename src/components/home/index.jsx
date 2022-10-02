88c982472fb465f96774682bfbd02313bd4b7bd5
Kriger6 <sasa.palinkas6@gmail.com>
2022-10-02 21:07:37 +0200

    more work done

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -4,24 +4,62 @@ import NavBar from '../navbar'
 import './index.css'
 import axios from 'axios'
 import { useRef } from 'react'
+import { useCallback } from 'react'
 
 const Home = ({ setState, state }) => {
 
   useEffect(() => {
     setState(true)
   }, [state, setState])
-
+  
+  
   const [buttonVisibility, setButtonVisibility] = useState(true)
   const [moveButtonGroup, setMoveButtonGroup] = useState(false)
   const [questionContainer, setQuestionContainer] = useState(false)
+  const [topics, setTopics] = useState(['Entertainment: Video Games', 'Science: Computers', 'General Knowledge', 'History'])
   const [selectedTopic, setSelectedTopic] = useState()
+  const [topicsVanish, setTopicsVanish] = useState(false)
+  
+  const fetchQuestions = useCallback(() => {
+    axios.get(`https://opentdb.com/api.php?amount=20&category=${selectedTopic}&difficulty=medium&type=multiple`)
+    .then((response) => console.log(response))
+  }, [selectedTopic])
+
+
+  useEffect(() => {
+    fetchQuestions()
+  }, [buttonVisibility, fetchQuestions])
+
 
   const topicList = useRef()
   
   const toggleButton = e => {
-    setSelectedTopic(prevState => e)
-    // console.log(e.target.innerHTML);
+    setSelectedTopic(e)
   }
+
+  const clearSelectedTopic = () => {
+      switch (selectedTopic) {
+        case 1:
+          setSelectedTopic(15)
+          break;
+        case 2:
+          setSelectedTopic(prevState => 18)
+          break;
+        case 3:
+          setSelectedTopic(prevState => 9)
+          break;
+        case 4:
+          setSelectedTopic(prevState => 23)
+          break;
+        default:
+          break;
+      }
+  }
+
+  const deleteTopics = () => {
+    setTopics(null)
+  }
+
 
   return (
     <div>
@@ -34,17 +72,24 @@ const Home = ({ setState, state }) => {
           <div className='topic-card'>
             {questionContainer && <div className={questionContainer ? 'question-container' : ''}></div>}
             <div ref={topicList} className={moveButtonGroup === true ? 'topic-button-container topic-button-container-animation' : 'topic-button-container'}>
-              <div className={selectedTopic === 1 ? 'topic-button something' : 'topic-button'} onClick={() => toggleButton(1)}>
-                Entertainment: Video Games
+              <div className={
+                `topic-button ${selectedTopic === 1 ? 'selected' : ''} ${topicsVanish === true ? 'topics-fade-away' : ''}`} onClick={() => toggleButton(1)}>
+                {topics && topics[0]}
               </div>
-              <div className={selectedTopic === 2 ? 'topic-button something' : 'topic-button'} onClick={() => toggleButton(2)}>
-                Science: Computers
+              <div className={
+                `topic-button ${selectedTopic === 2 ? 'selected' : ''} ${topicsVanish === true ? 'topics-fade-away' : ''}`
+              } onClick={() => toggleButton(2)}>
+                {topics && topics[1]}
               </div>
-              <div className='topic-button'>
-                General Knowledge
+              <div className={
+                `topic-button ${selectedTopic === 3 ? 'selected' : ''} ${topicsVanish === true ? 'topics-fade-away' : ''}`
+              } onClick={() => toggleButton(3)}>
+                {topics && topics[2]}
               </div>
-              <div className='topic-button'>
-                History
+              <div className={
+                `topic-button ${selectedTopic === 4 ? 'selected' : ''} ${topicsVanish === true ? 'topics-fade-away' : ''}`
+              } onClick={() => toggleButton(4)}>
+                {topics && topics[3]}
               </div>
             </div>
             <button className={buttonVisibility === true ? 'play-button' : 'play-button play-button-animation'} onClick={() => {
@@ -52,7 +97,11 @@ const Home = ({ setState, state }) => {
                 return
               }
               setButtonVisibility(false)
+              clearSelectedTopic()
+              setTopicsVanish(true)
+              fetchQuestions()
               setTimeout(() => {
+                deleteTopics()
                 setMoveButtonGroup(true)
                 setQuestionContainer(true)
               }, 1500)
